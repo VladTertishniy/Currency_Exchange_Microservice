dc25e3528e0d3e502e1a312c0166ce71809fce47
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-06-27 15:10:23 +0300

    removed package + delete unused imports

--- limitsMicroservice/src/main/java/com/extrawest/limitsmicroservice/service/LimitService.java
+++ limitsMicroservice/src/main/java/com/extrawest/limitsmicroservice/service/LimitService.java
@@ -1,15 +1,7 @@
 package com.extrawest.limitsmicroservice.service;
 
-import com.extrawest.limitsmicroservice.dto.request.LimitRequestDTO;
 import com.extrawest.limitsmicroservice.dto.response.LimitResponseDTO;
-
-import java.util.List;
 
 public interface LimitService {
     LimitResponseDTO getLimit();
-    /*LimitResponseDTO createLimit(LimitRequestDTO limitRequestDTO);
-    void deleteLimit(Long id);
-    LimitResponseDTO updateLimit(Long id, LimitRequestDTO limitRequestDTO);
-    List<LimitResponseDTO> getAllLimits();
-    LimitResponseDTO getLimitById(Long id);*/
 }
