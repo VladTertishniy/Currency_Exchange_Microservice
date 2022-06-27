05c7c3e719061707edb151538d3fddf5e3a82594
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-06-27 15:47:49 +0300

    some changes in pom.xml, application.properties and CurrencyExchangeMicroserviceApplication.java

--- currencyExchangeMicroservice/src/main/java/com/extrawest/currencyExchangeMicroservice/CurrencyExchangeMicroserviceApplication.java
+++ currencyExchangeMicroservice/src/main/java/com/extrawest/currencyExchangeMicroservice/CurrencyExchangeMicroserviceApplication.java
@@ -2,8 +2,14 @@ package com.extrawest.currencyExchangeMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableScheduling
 public class CurrencyExchangeMicroserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyExchangeMicroserviceApplication.class, args);
