4e843471a81fc6c001fd80ed2694857d9684cf43
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-01 16:30:51 +0300

    LimitController.java refactor, added configuration for config service for limits microservice

--- limitsMicroservice/src/main/java/com/extrawest/limitsmicroservice/controller/LimitController.java
+++ limitsMicroservice/src/main/java/com/extrawest/limitsmicroservice/controller/LimitController.java
@@ -5,11 +5,11 @@ import com.extrawest.limitsmicroservice.service.LimitService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/limits")
 @AllArgsConstructor
 public class LimitController {
 
